e5b7fd28a3f93b7c931e509223a9c237db7b4e3f
Rahma Dwi Ningsih <rahmadwi505@gmail.com>
2025-01-03 15:56:38 +0700

    perubahan user model

--- lib/models/user_profile_model.dart
+++ lib/models/user_profile_model.dart
@@ -6,17 +6,23 @@ part 'user_profile_model.g.dart';
 @freezed
 class UserProfile with _$UserProfile {
   factory UserProfile({
-    required String userId,
-    String? phoneNumber,
-    DateTime? dateOfBirth,
-    String? profilePictureUrl,
-    String? bio,
-    @Default([]) List<String> interests,
-    String? location,
-    DateTime? joinDate,
-    @Default({}) Map<String, dynamic> additionalInfo,
+    required String id,                
+    required String nama,              
+    required String email,            
+    required String nomor,                    
+    required String alamat,                   
+    required String kota,                      
+    required String provinsi,                 
+    required String kodePos,                  
+    required String logo,
   }) = _UserProfile;
 
   factory UserProfile.fromJson(Map<String, dynamic> json) =>
       _$UserProfileFromJson(json);
+}
+
+Map<String, dynamic> toJsonForCreate(UserProfile profile) {
+  final json = profile.toJson(); 
+  json.remove('id');
+  return json;
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:saldosmart/main.dart';
+import 'package:my_pml/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
